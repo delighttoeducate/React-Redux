48c7c34a922b659552ded92d9e3a282339162f9e
delighttoeducate <delighttoeducate@gmail.com>
2019-07-10 18:59:54 +0530

    Active,completed,all button handlers implemented

--- src/TodoApp/EachTask.js
+++ src/TodoApp/EachTask.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import styled,{css} from 'styled-components'
 class EachTask extends Component {
-    constructor(props) {
-        super(props)
-        this.handlerCheckbox=this.handlerCheckbox.bind(this)
-    }
-    
+  
     handlerCheckbox=()=>{
         alert("hello");
     }
 
     render() {
         const Span=styled.span`
-        text-decoration:${props=>props.taskStatus?'line-through':'none'};
+        text-decoration:${props=>props.taskStatus==='completed'?'line-through':'none'};
         `
         const Checkbox=styled.input`
         margin:auto;
@@ -22,7 +18,7 @@ class EachTask extends Component {
 
         return (
             <>
-                <Checkbox type='checkbox' onClick={()=>this.handlerCheckbox()} /><Span taskStatus={true}>{this.props.subject}</Span>
+                <Checkbox type='checkbox' onClick={()=>this.handlerCheckbox()} /><Span taskStatus={this.props.status}>{this.props.subject}</Span>
             </>
         )
     }
--- src/TodoApp/TodoApp.js
+++ src/TodoApp/TodoApp.js
@@ -1,74 +1,145 @@
-import React, { Component } from 'react'
-import styled,{css} from 'styled-components'
-import EachTask from './EachTask';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import EachTask from "./EachTask";
 export class TodoApp extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoItems: [
+        { id: 1, subject: "Welcome Task1", status: "active" },
+        { id: 2, subject: "Welcome Task2", status: "completed" },
+        { id: 3, subject: "Welcome Task3", status: "active" },
+        { id: 4, subject: "Welcome Task4", status: "completed" },
+        { id: 5, subject: "Welcome Task5", status: "active" }
+      ],
+      displayItems: []
+    };
+  }
+
+  componentDidMount() {
+    // Onload display All Items
+
+    this.setState({
+      displayItems: this.state.todoItems
+    });
+  }
+  render() {
+    // Styled Componets
+    const TodoHeader = styled.div`
+      margin: auto;
+      margin-top: 50px;
+      width: 50%;
+      border: 1px solid grey;
+      padding: 10px;
+      text-align: center;
+      background-color: blue;
+      font-weight: bold;
+      color: white;
+    `;
+    const Button = styled.button`
+      width: 95px;
+      height: 36px;
+      background-color: blue;
+      color: white;
+      font-weight: bold;
+      border: 1px solid grey;
+      margin: auto;
+    `;
+    const TodoWrapperBox = styled.div``;
+
+    const TodoArea = styled.div`
+      margin: auto;
+      margin-top: 10px;
+      width: 50%;
+      border: 1px solid grey;
+      padding: 10px;
+    `;
+
+    const TodoAdd = styled.div`
+      margin: auto;
+      width: 50%;
+      padding: 10px;
+      border: 1px solid grey;
+    `;
+
+    const TodoNavigate = styled.div`
+      margin: auto;
+      width: 50%;
+      padding: 10px;
+      border: 1px solid grey;
+      display: flex;
+      justify-content: space-between;
+    `;
+
+    // Handlers
+
     
-        this.state = {
-            todoItems:[  {id: 1, subject: "Welcome Task", status: true}]
-             
+    this.handlerClick = e => {
+      let tmpArr = [];
+
+      this.state.todoItems.filter(item => {
+        if (item.status === e) {
+          tmpArr.push(item);
         }
-    }
-    
-    render() {
-        const TodoHeader=styled.div`
-        margin:auto;
-        width:50%;
-        border:1px solid grey;
-        padding:10px;
+      });
+      if (e === "all") {
+        tmpArr = this.state.todoItems;
+      }
+      this.setState(
+        {
+          displayItems: tmpArr
+        },
+        () => {
+        //   console.log("State object evt: %O", this.state.displayItems);
+        }
+      );
+    };
 
-        
-        `
-        const TodoWrapperBox=styled.div`
-      
-        `
+    this.filterTodoItems = e => {};
+    return (
+      <div>
+        <TodoWrapperBox>
+          <TodoHeader>Todo List</TodoHeader>
+          <TodoArea>
+            {this.state.displayItems.map(eachItem => {
+              return (
+                <div>
+                  <EachTask
+                    key={eachItem.id}
+                    subject={eachItem.subject}
+                    status={eachItem.status}
+                  />
+                  <br />
+                </div>
+              );
+            })}
+          </TodoArea>
+          <TodoAdd>
+            <input type="text" width="100%" height="100%" />
+          </TodoAdd>
 
-        const TodoArea=styled.div`
-        margin:auto;
-        width:50%;
-        border:1px solid grey;
-        padding:10px;
-        `
-
-        const TodoAdd=styled.div`
-        margin:auto;
-        width:50%;
-        padding:10px;
-        border:1px solid grey;
-        `
-
-        const TodoNavigate=styled.div`
-        margin:auto;
-        width:50%;
-        padding:10px;
-        border:1px solid grey;
-        `
-        return (
-            <div>
-            
-               <TodoWrapperBox>
-                    <TodoHeader>
-                        Todo List
-                    </TodoHeader>
-                    <TodoArea>
-                    {this.state.todoItems.map((eachItem,index)=>{
-                        return <EachTask key={index} subject={eachItem.subject} status={eachItem.status}/>
-                    })}
-                    </TodoArea>
-                    <TodoAdd><input type='text' width='100%' height='100%'/></TodoAdd>
-
-                    <TodoNavigate>
-
-                        <button>All</button>
-                        <button>Active</button>
-                      <button>Completed</button>
-                    </TodoNavigate>
-
-               </TodoWrapperBox>
-            </div>
-        )
-    }
+          <TodoNavigate>
+            <Button location="left" onClick={() => this.handlerClick("all")}>
+              All
+            </Button>
+            <Button
+              location="margin:0 auto"
+              onClick={() => this.handlerClick("active")}
+            >
+              Active
+            </Button>
+            <Button
+              location="right"
+              onClick={() => this.handlerClick("completed")}
+            >
+              Completed
+            </Button>
+          </TodoNavigate>
+        </TodoWrapperBox>
+      </div>
+    );
+  }
 }
 
-export default TodoApp
+export default TodoApp;
